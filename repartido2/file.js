b87d54f56b975183b667438364b2c2a52712f95d
Marco Fonseca <89942525+Marcofonseca05@users.noreply.github.com>
2021-10-22 17:24:42 -0300

    Add files via upload

---
+++ repartido2/file.js
@@ -0,0 +1,70 @@
+/*/ Valor absoluto
+
+var num = parseInt(prompt ("Ingrese un numero"));
+
+if (num >=0) {
+    document.write( num);
+} else  {
+    document.write(  num *-1 + );
+}
+/*/
+/*/El mayor
+
+var num1 =parseInt(prompt ("Ingrese un numero"));
+var num2 =parseInt(prompt ("Ingrese un numero"));
+
+if (num1 > num2) {
+    document.write( num1 )
+} else {
+    alert(num2)
+}
+
+/*/
+
+/*etapas de lavida */
+/*/
+var edad = parseInt(prompt ("ingresar edad"));
+
+if (edad >= 0 && edad <= 2) {
+    alert("bebe")
+} 
+else if (edad > 2 && edad < 10){
+    alert("niño")
+}
+if (edad > 10 && edad < 15) {
+    alert("Preadolescente")
+} 
+else if (edad >= 15 && edad < 20){
+    alert("adolecente")
+}
+if (edad >= 20 && edad < 25) {
+    alert("joven adulto")
+} 
+else if (edad >= 25 && edad < 60){
+    alert("adulto")
+}
+if (edad >=60 && edad < 75) {
+    alert("Jubilado")
+} 
+else if (edad >= 75 && edad < 100){
+    alert("Adulto mayor")
+}
+/*/
+/* Socio del club*/
+/**
+var edad = parseInt(prompt ("Ingrese edad del socio"));
+var carne = confirm("tenes carnet al dia?" );
+
+if (edad >= 25 && carne== true) {
+    document.write("Puede votar")
+}
+else {
+    document.write("no puede votar")
+}
+*/
+
+
+
+
+
+
